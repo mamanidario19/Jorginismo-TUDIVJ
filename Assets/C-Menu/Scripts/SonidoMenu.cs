55dc37263838d19e8f5e6543a1e9c28c85a44ed4
Evelyn027 <eveh.rms7@gmail.com>
2022-11-14 14:42:35 -0300

    add-sounds-interface

---
+++ Assets/C-Menu/Scripts/SonidoMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidoMenu : MonoBehaviour {
+    public AudioSource sound;
+    public AudioClip soundHover;
+    public AudioClip soundClick;
+
+    public void SoundButtonHover()//Esta funcion es llamada desde el Boton a traves del componente Event Trigger.
+    {
+        //Eligimos que sonido sonara
+        sound.PlayOneShot (soundHover);
+        /* //Lo desactivo y activo para que genere el sonido
+        sound.enabled = false;
+        sound.enabled = true; */
+    }
+     public void SoundButtonClick()//Esta funcion es llamada desde el Boton a traves del componente Event Trigger.
+    {
+        //Eligimos que sonido sonara
+        sound.PlayOneShot (soundClick);
+    }
+
+}
